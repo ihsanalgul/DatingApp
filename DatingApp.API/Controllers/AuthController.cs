a2018f8d611443e7f2cfffadf5127abe7c860e95
rasim21 <rasimalgul@hotmail.com>
2020-02-13 19:38:39 +0300

    End of Section-5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -75,7 +75,9 @@ namespace DatingApp.API.Controllers
             return Ok(new
             {
                 token = tokenHandler.WriteToken(token)
-            });
+            }
+            );
+
         }
     }
 }
